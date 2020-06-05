1f0b474a9c756d8a115605ddb0c359a08dae5bb5
Saul Diosdado <sdbsns825@gmail.com>
2020-06-05 16:19:33 -0500

    Enables extension only when watching a YouTube video and added login button

--- background.js
+++ background.js
@@ -9,8 +9,13 @@ let accessToken = "";
 let tokenType = "";
 let tokenExpiresIn = "";
 
+// Login button event listener on the popup.html.
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('loginButton').addEventListener('click', implicitGrantLogin);
+});
+
 // Listens to changes in browser URL.
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if a YouTube video is being watched.
     if (regexYTVideoURL.exec(changeInfo.url)) {
         console.log("Watching a YouTube video!");
@@ -18,18 +23,40 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
     // Check if user was redirected to local host and verify if access was granted or denied.
     if (regexAccessGranted.exec(changeInfo.url)) {
+        // Set global query parameters from the response URL.
         let responseJSON = queryGrantedStringToJSON(changeInfo.url);
         accessToken = responseJSON.access_token;
         tokenType = responseJSON.token_type;
         tokenExpiresIn = responseJSON.expires_in;
+
+        // Alert the user of succesful authorization.
         alert("Access granted for " + tokenExpiresIn + " seconds!");
     } else if (regexAccessDenied.exec(changeInfo.url)) {
         alert("Access denied!");
     }
 });
 
+// Uses Chrome API declarativeContent to only show the popup.html content when watching a YouTube video.
+chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: "www.youtube.com/watch?*", schemes: ["https"]},
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+            css: ["video"]
+        })
+    ],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+}]);
+});
+
 // Occurs only when the extension is installed (or refreshed). Will be changed later.
 chrome.runtime.onInstalled.addListener(() => {
+    implicitGrantLogin();
+});
+
+// Redirect user to Spotify authorization screen with parameters.
+function implicitGrantLogin() {
     // Parameters for authorization.
     const authEndpoint = "https://accounts.spotify.com/authorize";
     const clientID = "a1064fa27beb43a38664d07aa5405304";
@@ -44,7 +71,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
     // Redirect to authorization screen where user accepts or denies permissions.
     window.open(authorizeURL, "_blank"); 
-});
+}
 
 // Convert an access granted URL hash fragment to a JSON.
 function queryGrantedStringToJSON(string) {
