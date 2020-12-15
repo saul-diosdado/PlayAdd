7c084a78981b765f29a9b62468b029a61f2e0f38
Saul Diosdado <sdbsns825@gmail.com>
2020-12-15 13:05:37 -0600

    Drop-down now populates with the user's created playlists.

--- popup.js
+++ popup.js
@@ -11,6 +11,7 @@ let coverArtElement = document.getElementById("track-art");
 let trackNameElement = document.getElementById("track-name");
 let artistNameElement = document.getElementById("artist-name");
 let trackPanelElement = document.getElementById("track-panel");
+let userPlaylistsElement = document.getElementById("playlists-dropdown");
 
 trackPanelElement.addEventListener("click", () => {
     chrome.storage.local.get("spotify_track_url", (item) => {
@@ -20,9 +21,13 @@ trackPanelElement.addEventListener("click", () => {
     });
 });
 
-// Retrieve the YouTube video title from storage and parse it.
+/**
+ * Retrieve the YouTube video title from storage and parse it to make a Spotify Search API call.
+ * Once done, get all of the user's playlists and update the UI.
+ */
 chrome.storage.local.get("yt_video_title", (item) => {
     spotifySearch(parseTitle(item.yt_video_title));
+    spotifyGetUserURI(spotifyGetUserPlaylists, spotifyUpdatePlaylistsUI);
 });
 
 // Call spotify API "search" endpoint which returns a list of tracks in JSON. 
@@ -81,6 +86,88 @@ function spotifyUpdateUI(coverURL, song, artist) {
     coverArtElement.src = coverURL;
     trackNameElement.innerText = song;
     artistNameElement.innerText = artist;
+}
+
+/**
+ * Since the Spotify API "Get Playlists" endpoint returns all playlists that the user has created and has followed,
+ * we must filter the playlists that only the user can modify (i.e. the playlists the user has created). To do this
+ * we need the user's URI and we can then compare the owner URI field found in each playlist Object.
+ * @param {function} callbackGetPlaylists Function that takes the user URI and filters only the user's playlists.
+ * @param {function} callbackUpdateUI Function that takes an array of playlists and populates the drop-down menu with the playlists.
+ */
+function spotifyGetUserURI(callbackGetPlaylists, callbackUpdateUI) {
+    const userEndpoint = "https://api.spotify.com/v1/me";
+    
+    chrome.storage.local.get("access_token", (item) => {
+        let xmlHTTP = new XMLHttpRequest();
+        xmlHTTP.open("GET", userEndpoint, true);
+        xmlHTTP.setRequestHeader("Authorization", "Bearer " + item.access_token);
+        xmlHTTP.onreadystatechange = () => {
+            if (xmlHTTP.readyState === 4 && xmlHTTP.status === 200) {
+                let userURI = JSON.parse(xmlHTTP.response).uri;
+                callbackGetPlaylists(userURI, callbackUpdateUI);
+            }
+        }
+        xmlHTTP.send();
+    });
+}
+
+/**
+ * Makes an API call to return the user's playlists. Filters only playlists that the user has created by comparing
+ * the userURI argument with the uri found in the playlist object.
+ * This function also updates the UI by passing the reponse to the callback fucntion.
+ * @param {string} userURI The Spotify user URI of the user that logged into Spotify.
+ * @param {function} callbackUpdateUI Function that takes an array of playlists and populates the drop-down menu with the playlists.
+ */
+function spotifyGetUserPlaylists(userURI, callbackUpdateUI) {
+    // Query parameters.
+    const playlistsEndpoint = "https://api.spotify.com/v1/me/playlists";
+
+    // Build the request URI.
+    const playlistsQuery = playlistsEndpoint;
+    
+    chrome.storage.local.get("access_token", (item) => {
+        let xmlHTTP = new XMLHttpRequest();
+        xmlHTTP.open("GET", playlistsQuery, true);
+        xmlHTTP.setRequestHeader("Authorization", "Bearer " + item.access_token);
+        xmlHTTP.onreadystatechange = () => {
+            if (xmlHTTP.readyState === 4 && xmlHTTP.status === 200) {
+                // An array of all playlists the user has (including ones they only follow).
+                let allUserPlaylists = JSON.parse(xmlHTTP.response).items;
+                console.log(allUserPlaylists);
+
+                // Filter the playlists retrieved from API to only show the playlists the user created.
+                let userCreatedPlaylists = [];
+                for (playlist of allUserPlaylists) {
+                    if (playlist.owner.uri == userURI) {
+                        userCreatedPlaylists.push(playlist);
+                    }
+                }
+
+                // Update the drop-down UI to show the user created playlists.
+                callbackUpdateUI(userCreatedPlaylists);
+            }
+        }
+        xmlHTTP.send();
+    });
+}
+
+/**
+ * Takes as parameter an array of the user's playlists, each of which is a playlist object.
+ * Adds an option in the drop-down menu UI for each of the user's playlist. 
+ * @param {array} playlists An array of JSON corresponding to the playlist object. 
+ */
+function spotifyUpdatePlaylistsUI(playlists) {
+    // Iterate through all of the user's playlists.
+    for (let i = 0; i < playlists.length; i++) {
+        // Construct the drop-down option.
+        let option = document.createElement("option");
+        option.value = playlists[i].uri;
+        option.text = playlists[i].name;
+
+        // Append the drop-down option to the drop-down menu.
+        userPlaylistsElement.appendChild(option);
+    }
 }
 
 /*--------------------------------------------------------------------------*/
