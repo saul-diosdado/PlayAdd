0921d6376f536471eea7a58881f7a0f7e6e54797
Saul Diosdado <sdbsns825@gmail.com>
2020-12-16 14:17:14 -0600

    Quick UI change.

--- login.js
+++ login.js
@@ -3,10 +3,6 @@
  * Purpose: Handles the login button and changing the popup if the user logged in.
  */
 
-/*--------------------------------------------------------------------------*/
-/* CONSTANTS */
-/*--------------------------------------------------------------------------*/
-
 // Tell the background.js script to start the login process when user hits "login".
 document.getElementById("login-button").addEventListener("click", () => {
     chrome.runtime.sendMessage({message: "login"});
